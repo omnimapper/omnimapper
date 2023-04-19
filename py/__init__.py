4e226ef55f9ef94ee88c3bcd14f988d9ae2b60cf
Txema Ynzenga <txemaynzenga@gmail.com>
2023-04-19 09:01:19 +0200

    Fix search path for non-timestamped module

--- py/__init__.py
+++ py/__init__.py
@@ -14,7 +14,7 @@ def loadmodule():
 		newest_so = max(glob.glob(''.join([os.path.dirname(__file__), '/omnimappercpp-t*'])), key=os.path.getctime)
 		so = ctypes.PyDLL(newest_so)
 	else:
-		so = ctypes.PyDLL('omnimappercpp')
+		so = ctypes.PyDLL(sum([glob.glob('omnimappercpp.' + e) for e in ['so', 'dll', 'dylib']], [])[0])
 
 	so.PyInit_omnimappercpp.argtypes = []
 	so.PyInit_omnimappercpp.restype = ctypes.py_object
